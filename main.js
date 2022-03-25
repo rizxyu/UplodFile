cd37eb0747fec5357c4f7a83a4f2bdec804f2622
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-03-25 11:55:30 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(__path + '/index.php')
+    res.sendFile(__path + '/index.html')
 })
 
 router.get('/config', (req, res) => {
