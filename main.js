a18c2ef806138ed77e49c4bad26c0c0e547682dd
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-03-25 11:48:53 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+__path = process.cwd()
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', (req, res) => {
+    res.sendFile(__path + '/views/index.html')
+})
+
+router.get('/config', (req, res) => {
+    config = {
+        status: true,
+        result: {
+            prefix : '/',
+            nama: 'Rizxyu',
+            instagram: 'MoonLyLy',
+            youtube : 'Moonly'
+        }
+    }
+    res.json(config)
+})
+
+module.exports = router
