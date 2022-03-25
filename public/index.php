a038e4769b4ab5cbd29b79937bdf406adbe3a5c4
ibrahKrep <100502146+ibrahKrep@users.noreply.github.com>
2022-03-25 21:04:42 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -16,7 +16,7 @@ function getFilesize($file) {
 <!DOCTYPE html>
 <html>
   <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <link rel="stylesheet" href="style.css">
     <title>Dragon File Uploader</title>
   </head>
