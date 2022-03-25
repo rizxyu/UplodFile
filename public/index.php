5d275e8773b1631b30828c1eda6012a143d951e8
ibrahKrep <100502146+ibrahKrep@users.noreply.github.com>
2022-03-25 20:43:50 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -43,6 +43,6 @@ function getFilesize($file) {
       ?>
     </div>
     <br>
-    <span><b>File Uploader By <a href="https://github.com/Rizxyu">Rizxyu</a> & <a href="https://github.com/MuhammadRestu999">MuhammadRestu999</a></b></span>
+    <span><b>File Uploader By <a href="https://github.com/Rizxyu">Rizxyu</a> & <a href="https://github.com/MuhammadRestu999">MuhammadRestu999</a> & <a href="https://github.com/ibrahKrep">ibrahKrep</a></b></span>
   </body>
 </html>
