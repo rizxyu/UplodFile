2c1906952200494d3921d223e94e1645e522f631
ibrahKrep <100502146+ibrahKrep@users.noreply.github.com>
2022-03-26 13:44:06 +0700

    location index.js

--- public/index.php
+++ public/index.php
@@ -45,5 +45,6 @@ function getFilesize($file) {
     </div>
     <br>
     <span><b>File Uploader By <a href="https://github.com/Rizxyu">Rizxyu</a> & <a href="https://github.com/MuhammadRestu999">MuhammadRestu999</a> & <a href="https://github.com/ibrahKrep">ibrahKrep</a></b></span>
+    <script src="index.js"></script>
   </body>
 </html>
